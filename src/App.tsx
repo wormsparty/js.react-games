9d8034f6ff745e810ea64386d8e122fea4165ac4
Marc-Olivier <marc-olivier.bloch@elca.ch>
2019-09-17 10:29:24 +0200

    Add button to switch view

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import './App.css';
 import Rogue from "./rogue/Rogue";
 import MapEdit from "./map/MapEdit";
 
 interface AppProps {
+}
+
+interface AppState {
     view: string
 }
 
-export default class App extends React.Component<AppProps, AppProps> {
-    static defaultProps: AppProps = { view: "Rogue" };
+const buttonStyle: CSSProperties = {
+    position: 'absolute',
+    left: 20,
+    top: 20,
+    fontSize: '30px',
+    userSelect: "none",
+    cursor: "pointer"
+};
+
+export default class App extends React.Component<AppProps, AppState> {
+    static defaultProps: AppState = { view: "Rogue" };
 
     constructor(props: AppProps) {
         super(props);
         this.state = App.defaultProps;
     }
 
+    switchToRogue = () => {
+        this.setState({ view: "Rogue" });
+    };
+
+    switchToMap = () => {
+        this.setState({ view: "MapEdit" });
+    };
+
     render() {
         return (
             <div>
+                <div style={buttonStyle}>
+                    <div onClick={this.switchToRogue}>
+                        🧙‍♂
+                    </div>
+                    <div onClick={this.switchToMap}>
+                        👩‍🎨
+                    </div>
+                </div>
                 {(this.state.view === "Rogue") && <Rogue />}
                 {(this.state.view === "MapEdit") && <MapEdit />}
             </div>
